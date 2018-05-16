ebcb529b30a5fb798d8b77bfef135ee4762d0743
Doug <dougstein@gmail.com>
2018-05-16 16:37:13 -0700

    CancelChain tests: ensure cancellation happens correctly for nested promise chains -- this relies on CancelContext.append(context: CancelContext) working as expected

---
+++ Tests/CorePromise/CancelChain.swift
@@ -0,0 +1,180 @@
+//
+//  CancelChain.swift
+//  CPKCoreTests
+//
+//  Created by Doug Stein on 5/16/18.
+//
+
+import XCTest
+import PromiseKit
+import CancelForPromiseKit
+
+class CancelChain: XCTestCase {
+    // Using a distinct type for each promise so we can tell which promise is which when using trace messages inside Thenable
+    struct A { }
+    struct B { }
+    struct C { }
+    struct D { }
+    struct E { }
+    
+    struct Chain {
+        let pA: Promise<A>
+        let pB: Promise<B>
+        let pC: Promise<C>
+        let pD: Promise<D>
+        let pE: Promise<E>
+    }
+    
+    func trace(_ message: String) {
+        // print(message)
+    }
+    
+    func cancelChainPromises(cancel context: CancelContext) -> Chain {
+        let pA: Promise<A> = Promise<A>(cancel: context) { seal in
+            self.trace("A IN")
+            afterCC(seconds: 0.01, cancel: context).done {
+                self.trace("A FULFILL")
+                seal.fulfill(A())
+            }.catch(policy: .allErrors) {
+                self.trace("A ERR")
+                seal.reject($0)
+            }
+        }
+        let pB: Promise<B> = Promise<B>(cancel: CancelContext()) { seal in
+            self.trace("B IN")
+            afterCC(seconds: 0.02, cancel: context).done {
+                self.trace("B FULFILL")
+                seal.fulfill(B())
+            }.catch(policy: .allErrors) {
+                self.trace("B ERR")
+                seal.reject($0)
+            }
+        }
+        let pC: Promise<C> = Promise<C>(cancel: CancelContext()) { seal in
+            self.trace("C IN")
+            afterCC(seconds: 0.03, cancel: context).done {
+                self.trace("C FULFILL")
+                seal.fulfill(C())
+           }.catch(policy: .allErrors) {
+                self.trace("C ERR")
+                seal.reject($0)
+            }
+        }
+        let pD: Promise<D> = Promise<D>(cancel: CancelContext()) { seal in
+            self.trace("D IN")
+            afterCC(seconds: 0.04, cancel: context).done {
+                self.trace("D FULFILL")
+                seal.fulfill(D())
+            }.catch(policy: .allErrors) {
+                self.trace("D ERR")
+                seal.reject($0)
+            }
+        }
+        let pE: Promise<E> = Promise<E>(cancel: CancelContext()) { seal in
+            self.trace("E IN")
+            afterCC(seconds: 0.05, cancel: context).done {
+                self.trace("E FULFILL")
+                seal.fulfill(E())
+            }.catch(policy: .allErrors) {
+                self.trace("E ERR")
+                seal.reject($0)
+            }
+        }
+        
+        return Chain(pA: pA, pB: pB, pC: pC, pD: pD, pE: pE)
+    }
+    
+    struct exABCDE {
+        let a: XCTestExpectation?
+        let b: XCTestExpectation?
+        let c: XCTestExpectation?
+        let d: XCTestExpectation?
+        let e: XCTestExpectation?
+        let cancelled: XCTestExpectation?
+    }
+    
+    func cancelChainSetup(waitTime: TimeInterval, ex: exABCDE) {
+        let context = CancelContext()
+        let c = cancelChainPromises(cancel: context)
+        
+        c.pA.thenCC { (_: A) -> Promise<A> in
+            self.trace("pA.then")
+            return firstlyCC { () -> Promise<B> in
+                self.trace("pB.firstly")
+                return c.pB
+            }.thenCC { (_: B) -> Promise<D> in
+                self.trace("pB.then")
+                return firstlyCC { () -> Promise<C> in
+                    self.trace("pC.firstly")
+                    ex.b?.fulfill() ?? XCTFail("pB.thenCC")
+                    return c.pC
+                }.thenCC { (_: C) -> Promise<D> in
+                    ex.c?.fulfill() ?? XCTFail("pC.thenCC")
+                    self.trace("pC.then")
+                    return c.pD
+                }
+            }.thenCC { (_: D) -> Promise<A> in
+                ex.d?.fulfill() ?? XCTFail("pD.doneCC")
+                return c.pA
+            }
+        }.thenCC { (_: A) -> Promise<E> in
+            self.trace("pA.then")
+            ex.a?.fulfill() ?? XCTFail("pA completed")
+            return c.pE
+        }.doneCC { _ in
+            ex.e?.fulfill() ?? XCTFail("pE completed")
+            self.trace("pE.done")
+        }.catchCC(policy: .allErrors) {
+            self.trace("Error: \($0)")
+            $0.isCancelled ? ex.cancelled?.fulfill() : print("Error: \($0)")
+        }
+        
+        after(seconds: waitTime).done {
+            context.cancel()
+        }
+        
+        let expectations = [ex.a, ex.b, ex.c, ex.d, ex.e, ex.cancelled].compactMap { $0 }
+        wait(for: expectations, timeout: 0.2)
+        return
+    }
+    
+    func testCancelChainPB() {
+        let ex = exABCDE(a: nil,
+                         b: expectation(description: "pB completed"),
+                         c: nil,
+                         d: nil,
+                         e: nil,
+                         cancelled: expectation(description: "cancelled"))
+        cancelChainSetup(waitTime: 0.025, ex: ex)
+    }
+
+    func testCancelChainPC() {
+        let ex = exABCDE(a: nil,
+                         b: expectation(description: "pB completed"),
+                         c: expectation(description: "pC completed"),
+                         d: nil,
+                         e: nil,
+                         cancelled: expectation(description: "cancelled"))
+        cancelChainSetup(waitTime: 0.035, ex: ex)
+    }
+
+    func testCancelChainPAD() {
+        let ex = exABCDE(a: expectation(description: "pB completed"),
+                         b: expectation(description: "pB completed"),
+                         c: expectation(description: "pC completed"),
+                         d: expectation(description: "pD completed"),
+                         e: nil,
+                         cancelled: expectation(description: "cancelled"))
+        cancelChainSetup(waitTime: 0.045, ex: ex)
+    }
+
+    func testCancelChainSuccess() {
+        let ex = exABCDE(a: expectation(description: "pA completed"),
+                         b: expectation(description: "pB completed"),
+                         c: expectation(description: "pC completed"),
+                         d: expectation(description: "pD completed"),
+                         e: expectation(description: "pE completed"),
+                         cancelled: nil)
+        cancelChainSetup(waitTime: 0.1, ex: ex)
+    }
+}
--- Tests/CorePromise/CancelTests.swift
+++ Tests/CorePromise/CancelTests.swift
@@ -6,20 +6,10 @@
 //
 
 import XCTest
-import CancelForPromiseKit
 import PromiseKit
+import CancelForPromiseKit
 
 class CancelTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
     func testAfter() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -263,5 +253,4 @@ class CancelTests: XCTestCase {
         
         wait(for: [exComplete], timeout: 1)
     }
-    
 }
