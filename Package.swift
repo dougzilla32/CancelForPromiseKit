a95706771b045f059cc0e080fb4bc083a04b3112
dougzilla <dougzilla32@gmail.com>
2018-05-31 22:43:49 -0700

    Update the Swift Package Manager files to use 'CancelForPromiseKit' (rather than the old 'CancellablePromiseKit')

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 
 import PackageDescription
 
-let pkg = Package(name: "CancellablePromiseKit")
+let pkg = Package(name: "CancelForPromiseKit")
 pkg.products = [
-    .library(name: "CancellablePromiseKit", targets: ["CancellablePromiseKit"]),
+    .library(name: "CancelForPromiseKit", targets: ["CancelForPromiseKit"]),
 ]
 
-let cpk: Target = .target(name: "CancellablePromiseKit")
+let cpk: Target = .target(name: "CancelForPromiseKit")
 cpk.path = "Sources"
 cpk.swiftLanguageVersions = [3, 4]
 cpk.targets = [
     cpk,
-    .testTarget(name: "A+", dependencies: ["CancellablePromiseKit"]),
+    .testTarget(name: "A+", dependencies: ["CancelForPromiseKit"]),
 ]
