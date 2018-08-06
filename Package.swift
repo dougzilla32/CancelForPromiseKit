1b87e83d0bc714f205f29dc3c61543ac37095997
Doug <dougstein@gmail.com>
2018-08-06 16:37:55 -0700

    SPM -- go back to using swift-tools-version:4.0 (rather than 3.0)

--- Package.swift
+++ Package.swift
@@ -1,9 +1,19 @@
+// swift-tools-version:4.0
 import PackageDescription
 
-let package = Package(
-    name: "CancelForPromiseKit",
-    dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6, minor: 3)
-    ],
-    swiftLanguageVersions: [3, 4]
-)
+let pkg = Package(name: "CancelForPromiseKit")
+pkg.products = [
+    .library(name: "CancelForPromiseKit", targets: ["CancelForPromiseKit"]),
+]
+pkg.dependencies = [
+    .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.3.4"),
+]
+
+let cpk: Target = .target(name: "CancelForPromiseKit")
+cpk.path = "Sources"
+cpk.dependencies = ["PromiseKit"]
+pkg.swiftLanguageVersions = [3, 4]
+pkg.targets = [
+    cpk,
+    .testTarget(name: "CPKCoreTests", dependencies: ["CancelForPromiseKit"], path: "Tests/CorePromise"),
+]
